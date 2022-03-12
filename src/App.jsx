95dfe95bcf6962382fa24253659f53a8a53f2834
João Victor Santos <victorrsilva2018@gmail.com>
2022-03-12 07:26:43 -0300

    Confirmar

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ import ScrollTop from './components/ScrollTop'
 export const App = () => {
   return (
     <>
-      <AppRouter/>
+      <AppRouter>
       <ScrollTop/>
+      </AppRouter>
+      
+      
     </>
   );
 };
