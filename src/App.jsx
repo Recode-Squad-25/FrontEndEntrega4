b5044ebad11a87d700e47563d907a9099651aca3
LeonardFLY <95866741+LeonardFLY@users.noreply.github.com>
2022-03-22 17:07:40 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import './styles/footer.css'
-import './styles/navbar.css'
+import React from 'react';
+import './styles/footer.css';
+import './styles/navbar.css';
 import AppRouter from './routes';
 import ScrollTop from './components/ScrollTop'
 
