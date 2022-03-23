21f4693c7b0d36e05a62fda03e6bdb23ed40cdff
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-03-22 23:42:42 -0300

    Adicionando card vagas - Estilo

--- src/components/CardVagasComponent.jsx
+++ src/components/CardVagasComponent.jsx
@@ -7,41 +7,41 @@ class CardVagasComponent extends Component {
 
     render() {
         return (
-            <div className='container'>
+            <div className='container text-white'>
                 <div className='row'>
 
-                    <div className="col" style={{ maxWidth: "22rem" }}>
-                        <div className="card">
+                    <div className="col" style={{ maxWidth: "22rem"}}>
+                        <div className="card" style={{ backgroundColor:"black"}}>
                             <img className="img-fluid card-img-top" alt="..." src={foto1} />
                             <div className="card-body">
                                 <h5 className="card-title">Venha fazer parte da Ipiranga  </h5>
                                 <p className="card-text"> Cargo : Trainee Ipiranga - Salario: R$ 6500</p>
                                 <p className="card-text"> Beneficios: Vale Transporte - Vale refeição - Plano de saúde </p>
-                                <button className="btn btn-info">Detalhes</button>
+                                <button className="btn btn-outline-warning vagas-btn">Detalhes</button>
                             </div>
                         </div>
                     </div>
 
                     <div className="col" style={{ maxWidth: "22rem" }}>
-                        <div className="card">
+                        <div className="card" style={{ backgroundColor:"black"}}>
                             <img className="img-fluid card-img-top" alt="..." src={foto2} />
                             <div className="card-body">
                                 <h5 className="card-title"> Não perca essa oprotunidade Trainee AG </h5>
                                 <p className="card-text"> Cargo : Trainee AG - Salario: R$ 3200</p>
                                 <p className="card-text"> Beneficios: Vale Transporte - Vale refeição </p>
-                                <button className="btn btn-info">Detalhes</button>
+                                <button className="btn btn-outline-warning vagas-btn">Detalhes</button>
                             </div>
                         </div>
                     </div>
 
                     <div className="col" style={{ maxWidth: "22rem" }}>
-                        <div className="card">
+                        <div className="card" style={{ backgroundColor:"black"}}>
                             <img className="img-fluid card-img-top" alt="..." src={foto3} />
                             <div className="card-body">
                                 <h5 className="card-title">Oprotunidade para Analista de sistemas</h5>
                                 <p className="card-text"> Cargo : Analista de sistemas Junior - Salario: R$ 4500</p>
                                 <p className="card-text"> Beneficios: Vale Transporte - Vale refeição  - Vale alimentação </p>
-                                <button className="btn btn-info">Detalhes</button>
+                                <button className="btn btn-outline-warning vagas-btn">Detalhes</button>
                             </div>
                         </div>
                     </div>
